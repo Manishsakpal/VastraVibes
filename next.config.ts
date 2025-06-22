1c8c42fb35b31734140326fdb6b7d2869f88357c
Manish anil Sakpal <sakpalmanish01@gmail.com>
2025-06-22 13:06:31 +0000

    remove every image urls
    and replace with this 3 url
    
    https://encrypted-

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.itl.cat',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   env: {
